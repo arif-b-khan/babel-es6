961d80d090186f9e815d4d19a6649c14e5ef36b3
Arif Khan <arif.bannehasan@gmail.com>
2020-04-03 22:59:05 +0530

    Adding babel-es6 starter project files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import c, {sub} from './mod1/Calculator';
+
+let result = c(10, 20);
+let subResult = sub(20, 10);
+console.log(`The subraction result is ${subResult}`);
+console.log(`The addition is ${result}`);
+

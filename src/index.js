29c4bc6724d40c6d8a929186593c0674fb257cf1
Arif Khan <arif.bannehasan@gmail.com>
2020-04-10 19:51:32 +0530

    Final version of javascript starter template

--- src/index.js
+++ src/index.js
@@ -2,15 +2,5 @@ import add from './mod1/Calculator';
 
 let result = add(1, 1);
 console.log(result);
-// function showThis(){
-//     console.log(this);
-//     console.log(Object.getPrototypeOf(this) === showThis.prototype);
-// }
 
-// showThis.prototype.testMethod = function(){
-//     console.log("this is test method");
-// }
-
-// let s = new showThis();
-// console.log(s);
 
